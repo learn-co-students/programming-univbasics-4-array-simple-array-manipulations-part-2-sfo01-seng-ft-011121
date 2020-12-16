0c25a90c0b24c0d6743f8b75f7ba9557a4c7bdda
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 23:05:30 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,24 +1,23 @@
 def using_concat(arr1, arr2)
-  return arr1.concat(arr2)
+  arr1.concat(arr2)
 end
 
 def using_insert(arr, ele)
-  return arr.insert(4, ele)
+  arr.insert(4, ele)
 end
 
 def using_uniq(arr)
-  return arr.uniq
+  arr.uniq
 end
 
 def using_flatten(arr)
-  return arr.flatten
+  arr.flatten
 end
 
 def using_delete(arr, str)
   arr.delete(str)
-  return arr
 end
 
-def using_delete_at(arr, i)
-  return arr.delete_at(i)
+def using_delete_at(arr, int)
+  arr.delete_at(int)
 end
