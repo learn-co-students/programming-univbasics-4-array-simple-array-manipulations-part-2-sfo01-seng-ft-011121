4728722fe9d6b468136a5159e2d87c3149060401
Carrie Bares <caroltbares@gmail.com>
2020-12-20 02:46:08 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_concat(array1, array2)
+  return array1.concat(array2)
+end
+
+def using_insert(array, ele)
+  array.insert(4, ele)
+  return array
+end
+
+def using_uniq(array)
+  return array.uniq
+end
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, n)
+  return array.delete_at(n)
+end
+
